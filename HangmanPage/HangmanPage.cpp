449b2dbf54651fc423acbd55abc5b5f7137c2601
Erfan Abedi <11455681+theerfan@users.noreply.github.com>
2018-01-01 22:37:26 +0330

    Actual Update #2.
    Seperated everything that needed to be seperated.
    Always checks for Q now.
    What to save is still unclear, left for further reviews.
    Added GeneralGamePlay.

--- HangmanPage/HangmanPage.cpp
+++ HangmanPage/HangmanPage.cpp
@@ -4,20 +4,26 @@
 #include "stdafx.h"
 #include <iostream>
 #include <fstream>
-#include <limits>
+#include <sstream>
 #include <string>
 #include <vector>
 #include <Windows.h>
+#include <ctime>
 
 using namespace std;
+
+bool didGiveUpOnTheGame = 0;
+string usedTopicAddress = "";
+
+// The Way this program saves player data is as follows. in the file of playername.txt we have:
+// FirstLine : Previous Score.
+// SecondLine : Formerly Played Subjects by name.
 
 COORD GetConsoleCursorPosition(HANDLE hConsoleOutput)
 {
 	CONSOLE_SCREEN_BUFFER_INFO cbsi;
 	if (GetConsoleScreenBufferInfo(hConsoleOutput, &cbsi))
-	{
 		return cbsi.dwCursorPosition;
-	}
 	else
 	{
 		// The function failed. Call GetLastError() for details.
@@ -26,14 +32,17 @@ COORD GetConsoleCursorPosition(HANDLE hConsoleOutput)
 	}
 }
 
-BOOL setxy(int x, int y)
+BOOL setxy(short x, short y)
 {
 	COORD c = { x,y };
 	return SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
 }
 
-void DrawRect(int x, int y, int width, int height, int curPosX = 0, int curPosY = 0)
+void DrawRect(int x, int y, int width, int height)
 {
+	HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+	COORD xy = GetConsoleCursorPosition(h);
+	short curPosX = xy.X, curPosY = xy.Y;
 	setxy(x, y); cout << char(201);
 	for (int i = 1; i < width; i++)
 		cout << char(205);
@@ -53,7 +62,7 @@ void DrawRect(int x, int y, int width, int height, int curPosX = 0, int curPosY 
 	setxy(curPosX, curPosY);
 }
 
-void DrawingTheGuy(short chances)
+void DrawingTheGuyPlusWrongGuesses(short numberOfWrongGuesses)
 {
 
 }
@@ -70,37 +79,53 @@ void ReadNthLine(string &line, ifstream &file, int n)
 	file >> line;
 }
 
-int main()
+int returnPreviousTotalScore(string playerName, char gameType)
 {
-	string playerName, line, topicsListAddress = "AVAILABLE_TOPICS.txt", selectedTopicAddress;
-	cout << "Enter your name:" << endl;
-	cin >> playerName;
-	char gameType;
-	int showCounter = 1, realCounter = 0, whichTopicNumber;
-	cout << "Type r for resuming your previous game, n for playing a new game" << endl;
-	cin >> gameType;
-	while ((gameType != 'r') || (gameType != 'c'))
-	{
-		cout << "Wrong input, please specify your orders again." << endl;
-		cin >> gameType;
-	}
+	if (gameType == 'n')
+		return 0;
+	ifstream playerFile(playerName + ".txt");
+	int n;
+	playerFile >> n;
+	return n;
+}
+
+vector <string> GenerateUnplayedSubjects(string playerName, char gameType)
+{
+	vector <string> gonnaReturn;
+	string topicsListAddress = "AVAILABLE_TOPICS.txt", line;
+	int showCounter = 1, gonnaRemove = 0;
 	ifstream listOfTopics(topicsListAddress);
-	cout << "Please choose the number of one of the following subjects:" << endl;
 	while (!listOfTopics.eof())
 	{
 		getline(listOfTopics, line);
-		cout << showCounter << ":  " << line << endl;
+		gonnaReturn.push_back(line);
+	}
+	if (gameType == 'r')
+	{
+		ifstream playerFile(playerName + ".txt");
+		getline(playerFile, line);
+		while (!playerFile.eof())
+		{
+			playerFile >> line;
+			for (string topics : gonnaReturn)
+			{
+				if (line == topics)
+					gonnaReturn.erase(gonnaReturn.begin() + gonnaRemove);
+				gonnaRemove++;
+			}
+		}
+	}
+	for (string topics : gonnaReturn)
+	{
+		cout << showCounter << ":  " << topics << endl;
 		showCounter++;
 	}
-	cin >> whichTopicNumber;
-	ReadNthLine(selectedTopicAddress, listOfTopics, whichTopicNumber);
-	ifstream selectedTopicFile(selectedTopicAddress);
-	vector <string> includedWords;
-	while (!selectedTopicFile.eof())
-	{
-		getline(selectedTopicFile, line);
-		includedWords.push_back(line);
-	}
+	return gonnaReturn;
+}
+
+void PlayOneSubjectUntilItsDone(string &playerName, char &gameType, vector<string> &includedWords, int &totalScore)
+{
+	string line;
 	int randomWordNumber = rand() % includedWords.size();
 	cout << endl << "Okay! Game will start in 3..2.." << endl;
 	system("cls");
@@ -110,7 +135,136 @@ int main()
 	for (int i = 0; i < currentWord.length(); i++)
 		cout << "_ ";
 	cout << endl << endl << "Now, LET THE GAMES BEGIN!" << endl;
+	short numberOfWrongGuesses = 0, numberofRightGuesses = 0;
+	int currentScore = 0;
+	char currentGuess;
+	bool hasGuessedRight;
+	string whatToShow = "";
+	while (numberOfWrongGuesses < 5)
+	{
+		hasGuessedRight = 0;
+		cout << "Enter your guess: ";
+		cin >> currentGuess;
+		if (currentGuess != 'Q')
+		{
+			for (char letter : currentWord)
+			{
+				if (letter == currentGuess)
+				{
+					whatToShow += letter + " ";
+					hasGuessedRight = 1;
+					numberofRightGuesses++;
+				}
+				else
+					whatToShow += "_ ";
+			}
+			cout << whatToShow << endl;
+			if (!hasGuessedRight)
+				numberOfWrongGuesses++;
+			if (numberofRightGuesses == currentWord.length())
+			{
+				currentScore = 3 * currentWord.length() - numberOfWrongGuesses;
+				cout << "CONGRATS! YOU HAVE FIGURED THE WORD OUT!" << endl;
+				break;
+			}
+			DrawingTheGuyPlusWrongGuesses(numberOfWrongGuesses);
+		}
+		else
+		{
+			didGiveUpOnTheGame = 1;
+			return;
+		}
+	}
+	totalScore += currentScore;
+}
 
+double PlayCalcScoreOfOneSubject(string playerName, char gameType)
+{
+	time_t starTimer = time(0);
+	string line, selectedTopicAddress, checkNotQ;
+	cout << "Please choose the number of one of the following subjects:" << endl;
+	vector <string> listOfUnplayedSubjects = GenerateUnplayedSubjects(playerName, gameType);
+	int whichTopicNumber;
+	cin >> checkNotQ;
+	if (checkNotQ != "Q")
+	{
+		stringstream sCheckNotQ(checkNotQ);
+		sCheckNotQ >> whichTopicNumber;
+		selectedTopicAddress = listOfUnplayedSubjects[whichTopicNumber];
+		usedTopicAddress = selectedTopicAddress;
+		listOfUnplayedSubjects.clear();
+		ifstream selectedTopicFile(selectedTopicAddress);
+		vector <string> includedWords;
+		while (!selectedTopicFile.eof())
+		{
+			selectedTopicFile >> line;
+			includedWords.push_back(line);
+		}
+		int totalScore = returnPreviousTotalScore(playerName, gameType);
+		while (!includedWords.empty())
+			PlayOneSubjectUntilItsDone(playerName, gameType, includedWords, totalScore);
+		//ReadNthLine(selectedTopicAddress, listOfTopics, whichTopicNumber);
+		time_t endTimer = time(0);
+		double finalScore = (double)(totalScore / difftime(endTimer, starTimer));
+		return finalScore;
+	}
+	else
+	{
+		didGiveUpOnTheGame = 1;
+		return -1;
+	}
+}
+
+void SavingShit()
+{
+
+}
+
+void GeneralGamePlay(string playerName, char gameType)
+{
+	do {
+		double totalScore = PlayCalcScoreOfOneSubject(playerName, gameType);
+		if (gameType == 'r')
+		{
+			ofstream playerFile;
+			playerFile.open(playerName + ".txt", ios_base::app);
+			playerFile << usedTopicAddress;
+		}
+		else
+		{
+			// Needs a full eval when it's specified.
+		}
+		cout << "Here's your total score on this subject: " << totalScore << endl << "Do you wish to continue? 1 for yes / 0 for no: ";
+		cin >> didGiveUpOnTheGame;
+	} while (!didGiveUpOnTheGame);
+	
+
+}
+
+int main()
+{
+	string playerName;
+	cout << "Enter your name:" << endl;
+	cin >> playerName;
+	char gameType;
+	if (playerName == "Q")
+		return 0;
+	cout << "Type r for resuming your previous game, n for playing a new game" << endl;
+	cin >> gameType;
+	if (gameType != 'Q')
+	{
+		while ((gameType != 'r') || (gameType != 'c'))
+		{
+			cout << "Wrong input, please specify your orders again." << endl;
+			cin >> gameType;
+			if (gameType == 'Q')
+			{
+				didGiveUpOnTheGame = 1;
+				return 0;
+			}
+		}
+	}
+	//currentScore = 0; 
     return 0;
 }
 
