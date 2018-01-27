25073224d64c377201243562de1f1bdf360a6714
Erfan Abedi <11455681+theerfan@users.noreply.github.com>
2018-01-27 03:42:41 +0330

    This is truly the last one.
    Improved graphics, added topic generator, added top10 section.
    [Was going to add a hint part, but it's too late now, hopefully graphics do the job.]

--- HangmanPage/HangmanPage.cpp
+++ HangmanPage/HangmanPage.cpp
@@ -6,8 +6,11 @@
 
 static const int CHARMAX = 1000;
 static char* TOPICSLIST = "AVAILABLE_TOPICS.TXT";
+static char* PLAYERSLIST = "PLAYERS.TXT";
 
 bool didGiveUpOnTheGame = 0;
+
+int previousWrongGuess = 0;
 
 void gotoxy(int x, int y)
 {
@@ -99,6 +102,14 @@ bool SearchInsideTheStringNode(char* searchee, StringNode* searched)
 			return 1;
 	if (!strcmp(current->string, searchee))
 		return 1;
+	return 0;
+}
+
+bool SearchInsideString(char* searchee, char searched)
+{
+	for (int i = 0; i < strlen(searchee); i++)
+		if (searched == searchee[i])
+			return 1;
 	return 0;
 }
 
@@ -207,20 +218,25 @@ SubjectNode* ReturnNthSubjectList(SubjectNode * head, int n)
 
 void DrawingTheGuy(short numberOfWrongGuesses)
 {
-	int i = 70, j = 4, lastX, lastY;
-	gotoxy(i, j);
-	if (numberOfWrongGuesses < 5)
+	HANDLE  hConsole;
+	hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	SetConsoleTextAttribute(hConsole, 8);
+	for (int i = 4; i < 26; i++)
 	{
+		gotoxy(58, i);
+		for (int j = 58; j < 95; j++)
+			printf("%c", ' ');
+	}
+	int i = 70, j = 4, lastX = 95, lastY = 25, firstX = 58, firstY;
+	{
+		gotoxy(i, j);
 		//Hairline is strong, bro.
 		gotoxy(i, j - 1);
 		for (int q = 0; q < 12; q++)
 			printf("%c", char(244));
-		lastX = i; lastY = j;
-	}
-	if (numberOfWrongGuesses < 4)
-	{
 		gotoxy(i, j);
 		// Draw the head
+		SetConsoleTextAttribute(hConsole, 14);
 		printf("%c", char(201));
 		for (int q = 0; q < 10; q++)
 			printf("%c", char(205));
@@ -246,26 +262,27 @@ void DrawingTheGuy(short numberOfWrongGuesses)
 			printf("%c", char(186));
 		}
 		//Draw the face
+		SetConsoleTextAttribute(hConsole, 9);
 		gotoxy(i + 2, j + 1);
 		printf("%c", char(233));
 		gotoxy(i + 8, j + 1);
 		printf("%c", char(233));
+		SetConsoleTextAttribute(hConsole, 14);
 		gotoxy(i + 5, j + 2);
 		printf("%c", char(186));
+		SetConsoleTextAttribute(hConsole, 8);
 		gotoxy(i + 2, j + 4);
 		for (int t = 0; t < 7; t++)
 			printf("%c", char(176));
-		lastX = i + 5; lastY = j + 7;
 		i += 6;
 		j += 7;
-	}
-	if (numberOfWrongGuesses < 3)
-	{
+		SetConsoleTextAttribute(hConsole, 14);
 		gotoxy(i - 3, j);
 		printf("%c", char(186));
 		gotoxy(i + 2, j);
 		printf("%c", char(186));
 		// Neck.
+		SetConsoleTextAttribute(hConsole, 13);
 		for (int t = -11; t < 12; t++)
 		{
 			gotoxy(i + t, j + 1);
@@ -304,6 +321,7 @@ void DrawingTheGuy(short numberOfWrongGuesses)
 				printf("%c", char(186));
 		}
 		// Main body.
+		SetConsoleTextAttribute(hConsole, 14);
 		for (int q = 0; q < 6; q++)
 		{
 			gotoxy(i + 13 + q, j + 3);
@@ -318,11 +336,8 @@ void DrawingTheGuy(short numberOfWrongGuesses)
 			gotoxy(i - 17 + q, j + 5);
 			printf("%c", char(205));
 		}
-		lastX = i + 1; lastY = j + 10;
 		//Hands.
-	}
-	if (numberOfWrongGuesses < 2)
-	{
+		SetConsoleTextAttribute(hConsole, 10);
 		gotoxy(i - 3, j + 10);
 		printf("%c", char(186));
 		gotoxy(i + 4, j + 10);
@@ -335,17 +350,12 @@ void DrawingTheGuy(short numberOfWrongGuesses)
 		printf("%c", char(186));
 		gotoxy(i + 4, j + 12);
 		printf("%c", char(186));
-		lastX = i + 1; lastY = j + 13;
 		//Legs
-	}
-	if (numberOfWrongGuesses < 1)
-	{
+		SetConsoleTextAttribute(hConsole, 12);
 		gotoxy(i - 3, j + 13);
 		printf("%c", char(185));
 		gotoxy(i - 6, j + 14);
 		printf("%c%c%c%c", char(200), char(205), char(205), char(188));
-		gotoxy(i + 4, j + 12);
-		printf("%c", char(186));
 		gotoxy(i + 4, j + 13);
 		printf("%c", char(204));
 		gotoxy(i + 4, j + 14);
@@ -354,16 +364,55 @@ void DrawingTheGuy(short numberOfWrongGuesses)
 		printf("%c%c%c", char(201), char(205), char(205));
 		gotoxy(i + 5, j + 13);
 		printf("%c%c%c", char(205), char(205), char(187));
-		lastX = i; lastY = j + 15;
 		//Feet
 	}
-
+	if (numberOfWrongGuesses == 5)
+		firstY = 2;
+	gotoxy(70, 2);
+	if (numberOfWrongGuesses == 4)
+		firstY = 9;
+	gotoxy(70, 8);
+	if (numberOfWrongGuesses == 3)
+		firstY = 11;
+	if (numberOfWrongGuesses == 2)
+		firstY = 19;
+	if (numberOfWrongGuesses == 1)
+		firstY = 23;
+	if (numberOfWrongGuesses == 0)
+		firstY = 25;
+	int sleepCounter;
+	SetConsoleTextAttribute(hConsole, 155);
+	for (; firstY != lastY; lastY--)
+	{
+		gotoxy(firstX, lastY);
+		for (int i = lastX; i >= firstX; i--)
+		{
+			printf("%c", char(247));
+			if ((((numberOfWrongGuesses == 5) && (lastY <= 9)) ||
+				((numberOfWrongGuesses == 4) && (lastY <= 11)) ||
+				((numberOfWrongGuesses == 3) && (lastY <= 19)) ||
+				((numberOfWrongGuesses == 2) && (lastY <= 23)) ||
+				((numberOfWrongGuesses == 1) && (lastY <= 25)) ||
+				(numberOfWrongGuesses == 0)) && (numberOfWrongGuesses != previousWrongGuess))
+				_sleep(5);
+		}
+		if ((((numberOfWrongGuesses == 5) && (lastY <= 9)) ||
+			((numberOfWrongGuesses == 4) && (lastY <= 11)) ||
+			((numberOfWrongGuesses == 3) && (lastY <= 19)) ||
+			((numberOfWrongGuesses == 2) && (lastY <= 23)) ||
+			((numberOfWrongGuesses == 1) && (lastY <= 25)) ||
+			(numberOfWrongGuesses == 0)) && (numberOfWrongGuesses != previousWrongGuess))
+			_sleep(5);
+	}
+	previousWrongGuess = numberOfWrongGuesses;
+	SetConsoleTextAttribute(hConsole, 15);
 }
 
 char* PlayOneSubjectUntilItsDone(char *playerName, char gameType, StringNode* includedWords, double* totalScore)
 {
 	srand(time(0));
 	char* line;
+	char wrongGuesses[6] = { ' ', ' ', ' ', ' ', ' ', ' ' };
 	int randomWordNumber = rand() % GetLinkedListSize(includedWords) + 1;
 	printf("\n Okay! Game will start in 3..2.. \n");
 	system("cls");
@@ -389,6 +438,10 @@ char* PlayOneSubjectUntilItsDone(char *playerName, char gameType, StringNode* in
 		printf("Now, the word consists of these letters, are you gonna be able to find them? \n%s", showBiz);
 		// Show current Status.
 		DrawingTheGuy(numberOfWrongGuesses);
+		gotoxy(0, 8);
+		printf("Your guesses thus far has been: \n");
+		for (int i = 1; i <= numberOfWrongGuesses; i++)
+			printf("%c ", wrongGuesses[i]);
 		gotoxy(0, 3);
 		printf("Enter your guess: ");
 		scanf(" %c", &currentGuess);
@@ -406,16 +459,20 @@ char* PlayOneSubjectUntilItsDone(char *playerName, char gameType, StringNode* in
 					checkBiz[i] = currentGuess;
 				}
 			}
-			if (!hasGuessedRight)
+			bool isDouble = SearchInsideString(wrongGuesses, currentGuess);
+			if ((!hasGuessedRight) && (!isDouble))
 				numberOfWrongGuesses++;
 			if (!strcmp(checkBiz, currentWord))
 			{
 				currentScore = 3 * strlen(currentWord) - numberOfWrongGuesses;
-				gotoxy(0, 4);
+				gotoxy(0, 5);
 				printf("CONGRATS! YOU HAVE FIGURED THE WORD OUT! \n");
-				_sleep(500);
+				printf("The word was: %s", currentWord);
+				_sleep(1000);
 				break;
 			}
+			else if ((!isDouble) && (!SearchInsideString(checkBiz, currentGuess)))
+				wrongGuesses[numberOfWrongGuesses] = currentGuess;
 		}
 		else
 		{
@@ -423,6 +480,13 @@ char* PlayOneSubjectUntilItsDone(char *playerName, char gameType, StringNode* in
 			return 0;
 		}
 	}
+	if (numberOfWrongGuesses == 5)
+	{
+		printf("YOU HAVE FAILED THIS CITY \n");
+		printf("The word was: %s", currentWord);
+		_sleep(1000);
+	}
+	DrawingTheGuy(numberOfWrongGuesses);
 	*totalScore += currentScore;
 	return currentWord;
 }
@@ -453,7 +517,16 @@ void SavingShit(char* playerName, SubjectNode* stars)
 			fprintf(playerFile, "%s   %lf\n", current->nameOfSubject, current->scoreOfSubject);
 		n++;
 	}
-	fclose(playerFile);
+}
+
+void SaveInPlayers(char* playerName, char gameType)
+{
+	if (gameType != 'r')
+	{
+		FILE* playersList = fopen(PLAYERSLIST, "a");
+		fprintf(playersList, "%s\n", playerName);
+		fclose(playersList);
+	}
 }
 
 SubjectNode* PlayCalcScoreOfOneSubject(char* playerName, char gameType)
@@ -532,10 +605,7 @@ void ClearShit(char* playerName, SubjectNode* stars)
 	for (current = head; current->next != NULL; current = current->next)
 		if (!strcmp(current->nameOfSubject,current->next->nameOfSubject))
 			head = RemoveElementFromSubjectNode(current->nameOfSubject, head);
-	SavingShit(playerName, head);
 }
-
-
 
 void GeneralGamePlay(char* playerName, char gameType)
 {
@@ -574,8 +644,96 @@ void GeneralGamePlay(char* playerName, char gameType)
 	{
 		SavingShit(playerName, allSubjects);
 		ClearShit(playerName, allSubjects);
+		SaveInPlayers(playerName, gameType);
 	}
 	return;
+}
+
+void TopicGenerator()
+{
+	char* a = (char*)malloc(sizeof(char*));
+	printf("Ok, give me the name of your to-be-created topic: ");
+	scanf(" %s", a);
+	FILE* mainList = fopen(TOPICSLIST, "a");
+	strcat(a, ".txt");
+	FILE *newList = fopen(a, "w");
+	fprintf(mainList, "\n%s", a);
+	printf("\n Now, add your chosen words and at the end of them, enter 0. \n");
+	while (scanf("%s", a) > 0)
+	{
+		if (!strcmp(a, "0"))
+			break;
+		fprintf(newList, "%s  ", a);
+	}
+}
+
+void ShowTopTen()
+{
+	int howManyPlayers = 1;
+	char* line = (char*)malloc(sizeof(char*));
+	double dees;
+	FILE * listOfPlayers = fopen(PLAYERSLIST, "r");
+	StringNode *gonnaReturn = (StringNode*)malloc(sizeof(StringNode*));
+	SubjectNode* allPlayersSumDouble = (SubjectNode*)malloc(sizeof(SubjectNode*));
+	fscanf(listOfPlayers, "%s", line);
+	char* q = (char*)malloc(sizeof(char*));
+	strcpy(q, line);
+	gonnaReturn->string = q;
+	gonnaReturn->next = NULL;
+	while (fscanf(listOfPlayers, "%s", line) > 0)
+	{
+		char* q = (char*)malloc(sizeof(char*));
+		strcpy(q, line);
+		AddStringNodeBottom(q, gonnaReturn);
+		howManyPlayers++;
+	}
+	SubjectNode *head = (SubjectNode*)malloc(sizeof(SubjectNode*));
+	char* name = ReturnNthLinkedListNumber(gonnaReturn, 1);
+	FILE * tempPlayer = fopen(name, "r");
+	double sum = 0;
+	while (fscanf(tempPlayer, "%s %lf", line, &dees) > 0)
+		sum += dees;
+	head->nameOfSubject = (char*)malloc(sizeof(char*));
+	strcpy(head->nameOfSubject, name);
+	head->scoreOfSubject = sum;
+	head->next = NULL;
+	for (int i = 2; i <= howManyPlayers; i++)
+	{
+		name = ReturnNthLinkedListNumber(gonnaReturn, i);
+		FILE * tempPlayer = fopen(name, "r");
+		sum = 0;
+		while (fscanf(tempPlayer, "%s %lf", line, &dees) > 0)
+			sum += dees;
+		AddSubjectNodeBottom(name, sum, head);
+	}
+	int nPla = howManyPlayers;
+	char* goat = (char*)malloc(sizeof(char*));
+	double maxScore = 0;
+	while (nPla > 1)
+	{
+		SubjectNode* kk = ReturnNthSubjectList(head, 1);
+		maxScore = 0;
+		goat = (char*)malloc(sizeof(char*));
+		while (kk != NULL)
+		{
+			double tempScore = kk->scoreOfSubject;
+			if (tempScore > maxScore)
+			{
+				maxScore = tempScore;
+				strcpy(goat, kk->nameOfSubject);
+			}
+			kk = kk->next;
+		}
+		head = RemoveElementFromSubjectNode(goat, head);
+		printf("%d. %s with the total score of: %lf \n", howManyPlayers - nPla + 1, goat, maxScore);
+		nPla--;
+	}
+	if (howManyPlayers != 10)
+	{
+		SubjectNode* kk = ReturnNthSubjectList(head, 1);
+		printf("%d. %s with the total score of: %lf \n", howManyPlayers - nPla + 1, kk->nameOfSubject, kk->scoreOfSubject);
+	}
+	_sleep(10000);
 }
 
 int main()
@@ -586,21 +744,27 @@ int main()
 	char* gameType1 = (char*)malloc(sizeof(char*));
 	if (playerName == "Q")
 		return 0;
-	printf("Type r for resuming your previous game, n for playing a new game: ");
+	printf("Type r for resuming your previous game, n for playing a new game, t for topic generator, o for showing top ten: ");
 	scanf(" %s", gameType1);
 	char gameType = gameType1[0];
 	if (gameType != 'Q')
 	{
-		while ((gameType != 'r') && (gameType != 'n'))
+		while ((gameType != 'r') && (gameType != 'n') && (gameType != 't') && (gameType != 'o'))
 		{
-			scanf("Wrong input, please specify your orders again. \n %c", gameType);
+			printf("Wrong input, please specify your orders again. \n");
+			scanf(" %c", &gameType);
 			if (gameType == 'Q')
 			{
 				didGiveUpOnTheGame = 1;
 				return 0;
 			}
 		}
-		GeneralGamePlay(playerName, gameType);
+		if ((gameType == 'r') || (gameType == 'n'))
+			GeneralGamePlay(playerName, gameType);
+		else if (gameType == 't')
+			TopicGenerator();
+		else
+			ShowTopTen();
 	}
 	//currentScore = 0; 
 	return 0;
